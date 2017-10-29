410dfc7f25eb8e8e03e629f504dc9ba2c40abb2c
reda <ridaahyad@gmail.com>
2017-10-29 16:47:16 +0000

    commit after long time

--- Exercices/Exercices/Exercices.iOS/Exercices/AbsolutePage.cs
+++ Exercices/Exercices/Exercices.iOS/Exercices/AbsolutePage.cs
@@ -1,8 +1,13 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Exercices
 {
     internal class AbsolutePage : Page
     {
+        public static implicit operator Page(AbsolutePage v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
